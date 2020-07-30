e84940acd6b758f6a3c628d09c347d741bfbdfbb
thrwwy216 <thrwwy216@tutanota.com>
2020-07-29 21:56:36 -0500

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/kwgdev/sfgpetclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package com.kwgdev.sfgpetclinic.model;
+
+/**
+ * created by kw on 7/31/2020 @ 3:03 PM
+ */
+public class Owner extends Person {
+}
---
+++ src/main/java/com/kwgdev/sfgpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package com.kwgdev.sfgpetclinic.model;
+
+/**
+ * created by kw on 7/31/2020 @ 3:04 PM
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
