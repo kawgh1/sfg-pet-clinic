36724cf8cfdd91bf4540ff29ab882d17e78d41f0
thrwwy216 <thrwwy216@tutanota.com>
2020-08-02 23:56:44 -0500

    Adding Index Page and Controller. Closes #8.

---
+++ pet-clinic-web/src/main/java/com/kwgdev/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.kwgdev.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * created by kw on 8/2/2020 @ 11:45 PM
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
