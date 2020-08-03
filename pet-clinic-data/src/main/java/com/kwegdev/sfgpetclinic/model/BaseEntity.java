a71a8d79d914c70d840f0e62f2cb2891242fee3c
thrwwy216 <thrwwy216@tutanota.com>
2020-08-02 20:57:45 -0500

    adding BaseEntity class. Closes #10.

---
+++ pet-clinic-data/src/main/java/com/kwegdev/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,19 @@
+package com.kwegdev.sfgpetclinic.model;
+
+import java.io.Serializable;
+
+/**
+ * created by kw on 8/2/2020 @ 8:54 PM
+ */
+public class BaseEntity implements Serializable {
+
+    private Long id; // Hibernate prefers to use Box type (Long vs long) because Box types can't be null
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
