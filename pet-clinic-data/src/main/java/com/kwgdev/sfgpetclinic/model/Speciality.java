560f2fc5a99d3df0598ecb1d121b2da9077c5508
thrwwy216 <thrwwy216@tutanota.com>
2020-08-11 09:45:10 -0500

    Create Vet Speciality, added to Vet. Closes #27.

---
+++ pet-clinic-data/src/main/java/com/kwgdev/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,17 @@
+package com.kwgdev.sfgpetclinic.model;
+
+/**
+ * created by kw on 8/11/2020 @ 9:43 AM
+ */
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/kwgdev/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/kwgdev/sfgpetclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package com.kwgdev.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * created by kw on 7/31/2020 @ 3:02 PM
  */
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
