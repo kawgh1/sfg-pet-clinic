eee9e70982e0eac1655838c6d241adc61ceb1b28
thrwwy216 <thrwwy216@tutanota.com>
2020-08-11 10:00:30 -0500

    Added PetTypeService Map closes #29.

---
+++ pet-clinic-data/src/main/java/com/kwgdev/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package com.kwgdev.sfgpetclinic.services;
+
+import com.kwgdev.sfgpetclinic.model.PetType;
+
+/**
+ * created by kw on 8/11/2020 @ 9:50 AM
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/kwgdev/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,41 @@
+package com.kwgdev.sfgpetclinic.services.map;
+
+import com.kwgdev.sfgpetclinic.model.PetType;
+import com.kwgdev.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * created by kw on 8/11/2020 @ 9:51 AM
+ */
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
