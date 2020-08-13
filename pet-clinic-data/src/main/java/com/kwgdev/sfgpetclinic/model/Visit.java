c9a13a794d2c7247234fe6a78d3608579a220221
thrwwy216 <thrwwy216@tutanota.com>
2020-08-12 21:47:38 -0500

    updated Visit Entity with JPA  mappings. Closes #38.

--- pet-clinic-data/src/main/java/com/kwgdev/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/kwgdev/sfgpetclinic/model/Visit.java
@@ -1,14 +1,22 @@
 package com.kwgdev.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * created by kw on 8/11/2020 @ 9:40 AM
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id") // a pet can have many visits
     private Pet pet;
 
     public LocalDate getDate() {
