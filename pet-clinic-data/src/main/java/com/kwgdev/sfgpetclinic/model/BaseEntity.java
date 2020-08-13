33cfc5280d5a307fd2905e702c6c5d74f1d20fba
thrwwy216 <thrwwy216@tutanota.com>
2020-08-12 20:50:04 -0500

    Updated base entity with JPA annotation. Closes #35.

--- pet-clinic-data/src/main/java/com/kwgdev/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/kwgdev/sfgpetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package com.kwgdev.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * created by kw on 8/2/2020 @ 8:54 PM
  */
+@MappedSuperclass // tells JPA this is a base class and other classes will be inheriting from it, so don't map it to the database
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Hibernate prefers to use Box type (Long vs long) because Box types can't be null
 
     public Long getId() {
