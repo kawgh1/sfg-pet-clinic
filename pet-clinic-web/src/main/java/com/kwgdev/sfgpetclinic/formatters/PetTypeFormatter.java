aa6c607d7e8ad577ddf5cbaf61ca524b76294306
thrwwy216 <thrwwy216@tutanota.com>
2020-08-16 14:53:10 -0500

    Added Visit Controller and mappings. Closes #57.

---
+++ pet-clinic-web/src/main/java/com/kwgdev/sfgpetclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,43 @@
+package com.kwgdev.sfgpetclinic.formatters;
+
+import com.kwgdev.sfgpetclinic.model.PetType;
+import com.kwgdev.sfgpetclinic.services.PetTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+/**
+ * created by kw on 8/16/2020 @ 2:25 PM
+ */
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    private final PetTypeService petTypeService;
+
+    @Autowired
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+}
